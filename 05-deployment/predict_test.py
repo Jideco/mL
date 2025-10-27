ac18505ea44dad11ace8e73807699d2e88f0abd3
Jideco <49160448+Jideco@users.noreply.github.com>
2025-10-27 16:46:51 +0000

    predict_script

---
+++ 05-deployment/predict_test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import requests
+
+url = 'http://localhost:9696/predict'
+
+customer = {
+    "gender": "female",
+    "seniorcitizen": 0,
+    "partner": "yes",
+    "dependents": "no",
+    "phoneservice": "no",
+    "multiplelines": "no_phone_service",
+    "internetservice": "dsl",
+    "onlinesecurity": "no",
+    "onlinebackup": "yes",
+    "deviceprotection": "no",
+    "techsupport": "no",
+    "streamingtv": "no",
+    "streamingmovies": "no",
+    "contract": "month-to-month",
+    "paperlessbilling": "yes",
+    "paymentmethod": "electronic_check",
+    "tenure": 1,
+    "monthlycharges": 29.85,
+    "totalcharges": 29.85
+}
+
+
+#requests.post(url, json=customer).json()
+
+response = requests.post(url, json=customer).json()
+
+
+if response['churn'] == True:
+    print('Sending promo email to %s' % ('jide-123'))
+else:
+    print('Not sending promo emial to %s' % ('jide-123'))
